a52feec4420e129a2958c9b76bcfc37238dc8200
sasarock <sasararock@gmail.com>
2016-12-28 22:38:46 +0900

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,12 +8,22 @@ class GroupsController < ApplicationController
    def new
      @group = Group.new
    end
-    def show
-   @group = Group.find(params[:id])
- end
+   def show
+     @group = Group.find(params[:id])
+     @posts = @group.posts
+   end
  def edit
 end
+def create
+  @group = Group.new(group_params)
+  @group.user = current_user
 
+  if @group.save
+    redirect_to groups_path
+  else
+    render :new
+  end
+end
 def update
   if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
